661c8006b7474b9040c202a7d8b3ecb46975962b
Kennedy Junior <josekennedy37@gmail.com>
2025-05-29 20:24:19 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import customtkinter as ctk
 from PIL import Image, ImageTk
 janela = ctk.CTk()
-janela.geometry("600x500")
 janela.geometry("500x400")
 janela.title("Codificador Hash")
 
@@ -22,8 +21,6 @@ def alternar_tema():
     if tema_atual == "Dark":
         ctk.set_appearance_mode("Light")
         tema_atual = "Light"
-        btn_tema.configure(image=img_sol, text="Light mode", text_color="black", fg_color="#ACA8A8")
-        btn.configure(text_color="black")
         btn_tema.configure(image=img_sol, text="Light mode", text_color="black", fg_color="#ACA8A8",
                            hover_color="#505050", border_color="#000000")
         btn.configure(text_color="black", hover_color="#505050", border_color="black", fg_color="#ACA8A8")
@@ -31,8 +28,6 @@ def alternar_tema():
     else:
         ctk.set_appearance_mode("Dark")
         tema_atual = "Dark"
-        btn_tema.configure(image=img_lua, text="Dark mode", text_color="white", fg_color="#333030")
-        btn.configure(text_color="white")
         btn_tema.configure(image=img_lua, text="Dark mode", text_color="white", fg_color="#333030",
                            hover_color="#6B1CB4", border_color="#6B1CB4")
         btn.configure(text_color="white", hover_color="#6B1CB4", border_color="#6B1CB4",
@@ -46,11 +41,9 @@ btn_tema = ctk.CTkButton(janela, text="Dark mode", image=img_lua, fg_color="#333
 btn_tema.place(relx=1.0, rely= 0.0, anchor= "ne", x=-10, y=10)
 
 
-btn = ctk.CTkButton(master=janela, text="Botão", font=("Bold", 20), corner_radius=32, fg_color="transparent",
 btn = ctk.CTkButton(master=janela, text="Codificar", font=("Bold", 20), corner_radius=32, fg_color="transparent",
                     hover_color="#6B1CB4", border_color="#6B1CB4",
                     border_width=2)
-btn.place(relx=0.5, rely=0.5, anchor="center")
 btn.place(x=350, y=100)
 
 frame_txt_principal = ctk.CTkFrame(janela, border_width=2, border_color="#6B1CB4", fg_color="transparent",)
